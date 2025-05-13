908afe21495eb5c318a4b396c2c7fedfe9e43162
Deekshigowda08 <149522963+Deekshigowda08@users.noreply.github.com>
2025-05-13 22:55:05 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,103 @@
-import Image from "next/image";
+"use client"
+import React, { useEffect, useState } from 'react';
+import { FaShoelace } from "react-icons/fa6";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { FaRegCircleUser } from "react-icons/fa6";
 
-export default function Home() {
+const App = () => {
+  const images = [
+    'image/adidas.jpg?text=Slide+1',
+    'image/puma.jpg?text=Slide+2',
+    'image/crocs.jpg?text=Slide+3',
+    'image/shoes.jpg?text=Slide+4',
+  ];
+
+  const [current, setCurrent] = useState(0);
+
+  const nextSlide = () => {
+    setCurrent((prev) => (prev + 1) % images.length);
+  };
+
+  const prevSlide = () => {
+    setCurrent((prev) => (prev - 1 + images.length) % images.length);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      nextSlide();
+    }, 10000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-[#fef5e4] font-sans text-center">
+      {/* Header */}
+      <header className="py-6 px-2">
+        <div className="flex items-center justify-between mb-4">
+          <div className="flex items-center justify-center">
+            <FaShoelace className="text-4xl text-black" />
+            <h1 className="text-4xl text-green-800 font-serif font-bold ml-2">StrideCraft</h1>
+          </div>
+          <div className="flex items-center justify-center">
+            <FaRegCircleUser className="text-3xl text-black" />
+            <MdOutlineShoppingCart className="text-3xl ml-4 text-black" />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Slideshow */}
+        <div className="relative w-full max-w-3xl mx-auto overflow-hidden rounded-lg shadow-lg mt-6">
+          <div className="relative h-64 sm:h-96">
+            <div
+              className="flex transition-transform duration-500 ease-in-out"
+              style={{ transform: `translateX(-${current * 100}%)` }}
+            >
+              {images.map((img, index) => (
+                <img
+                  key={index}
+                  src={img}
+
+                  alt={`Slide ${index + 1}`}
+                  className="w-full h-full md:p-20 md:pt-0 object-cover lg:object-center"
+                />
+              ))}
+            </div>
+          </div>
+
+          {/* Buttons */}
+          <button
+            onClick={prevSlide}
+            className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white px-4 py-2 rounded hover:bg-opacity-75 transition"
+          >
+            &#10094;
+          </button>
+          <button
+            onClick={nextSlide}
+            className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-black bg-opacity-50 text-white px-4 py-2 rounded hover:bg-opacity-75 transition"
+          >
+            &#10095;
+          </button>
+        </div>
+      </header>
+
+      {/* Categories */}
+      <section className="bg-green-800 text-white py-8 px-4">
+        <h2 className="text-3xl font-bold font-serif mb-6">Categories</h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-6 max-w-4xl mx-auto">
+          {[
+            { name: 'Crocs', img: '/image/crocsdisplay.png' },
+            { name: 'Puma', img: '/image/pumashoes.png' },
+            { name: 'Nike', img: '/image/nikeshoes.png' },
+            { name: 'Adidas', img: '/image/adidasshoes.png' },
+          ].map((item) => (
+            <button onClick={()=>{window.location.assign(`/${item.name}`)}} key={item.name} className="bg-white  p-2 border-2 border-black rounded-xl shadow-2xl">
+              <img src={item.img} alt={item.name} className="w-30 h-30 mx-auto" />
+              <p className="text-black text-2xl font-sans font-bold">{item.name}</p>
+            </button>
+          ))}
+        </div>
+      </section>
     </div>
   );
-}
+};
+
+export default App;
